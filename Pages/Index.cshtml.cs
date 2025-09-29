a52f97181415267ecc9922ac137dc25d011b8fa3
LuckyZhangWeiwei <zhangzhang_10@163.com>
2025-09-29 19:13:57 +0800

    add configuration test

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,8 +5,14 @@ namespace azure_app_trev.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public IndexModel(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
     public void OnGet()
     {
-
+        ViewData["Greeting"] = _configuration["Greeting"];
     }
 }
